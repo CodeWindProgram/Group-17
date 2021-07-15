853085bce7c8f1d3881d5ab7c5b0588e7838045f
123456nono12345 <86613358+123456nono12345@users.noreply.github.com>
2021-07-15 18:04:01 +0530

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,42 @@
+import React from "react";
+import "../node_modules/bootstrap/dist/css/bootstrap.min.css";
+import "../node_modules/bootstrap/dist/js/bootstrap.bundle";
+import Dashboard from "./Dashboard";
+import ViewReport from "./ViewReport";
+import Exams from "./Exams";
+
+
+
+
+
+import Navbar from "./Navbar";
+
+
+import { Route, } from "react-router-dom";
+
+function App() {
+  return (
+    <>
+      <Navbar />
+      
+      <switch>
+      
+        <Route exact path="/Dashboard" component={Dashboard} />
+        <Route exact path="/Exams" component={Exams} />
+        <Route exact path="/ViewReport" component={ViewReport} />
+     
+      
+
+
+
+      </switch>
+      
+     
+
+
+
+    </>
+  );
+}
+
+export default App;
